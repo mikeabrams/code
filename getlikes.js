001c16cc39061fbd6e72f173f2f2fe7d7b836d96
Mike <34291530+mikeabrams@users.noreply.github.com>
2019-06-05 22:49:53 -0700

    Update getlikes.js

--- getlikes.js
+++ getlikes.js
@@ -9,48 +9,6 @@ var finallist = [];
 setCommand('getlikers', startLikes, 'Started collecting likers PRESS g TO GET');
 setCommand('printlikers', stopLikes, 'Stoped collecting likers and output');
 
-setCommand('setpage', setPage, 'Set Page');
-setCommand('removeprivate', removePrivate, 'Check');
-
-function setPage() {
-  document.open();
-  document.write('<iframe name="iFrameName"></iframe>');
-  document.close();
-}
-
-function loadUrl(url) {
-  //'https://www.instagram.com/susan.colman/'; sleep(1000)
-  document.getElementsByName('iFrameName')[0].src = url;
-}
-
-function isPrivate() {
-  var privateAccount = false;
-  var iframe = document.getElementsByName('iFrameName')[0];
-  var h2s = iframe.contentWindow.document.getElementsByTagName('h2');
-  for (var i = 0; i < h2s.length; i++) {
-    var h2 = h2s[i].innerHTML;
-    if (h2 === 'This Account is Private') {
-      privateAccount = true;
-    }
-  }
-  if (privateAccount) {
-    return true;
-  }
-}
-
-function removePrivate() {
-  finallist = [];
-  console.log('Started');
-  for (var i = 0; i < likerslist.length; i++) {
-    loadUrl(likerslist[i]);
-    sleep(3000);
-    if (!isPrivate(likerslist[i])) {
-      finallist.push(likerslist[i]);
-    }
-  }
-  console.log('Done');
-}
-
 function stopLikes() {
   window.removeEventListener('sckeypressroll', function() {});
   doc = [];
@@ -92,9 +50,4 @@ function setCommand(cmd, func, msg) {
       return func();
     }
   });
-}
-
-function sleep(miliseconds) {
-  var currentTime = new Date().getTime();
-  while (currentTime + miliseconds >= new Date().getTime()) {}
 }
